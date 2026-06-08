19b5a0bb7a677c99383c325365bd0cb356817825
“Anoud <anoudalsaidi1993@gmail.com>
2026-06-08 16:02:03 +0400

    add booking cancellation and completion features

--- Hotel Management System/Services/BookingService.cs
+++ Hotel Management System/Services/BookingService.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Hotel_Management_System.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,20 @@ namespace Hotel_Management_System.Services
 {
    public class BookingService
     {
+        public static void DisplayAllBookings()
+        {
+            List<BookingModel> booking = new List<BookingModel>();
+
+            foreach(BookingModel book in booking)
+            {
+                Console.WriteLine($"booking Id: {book.bookingId}," +
+                    $"guest Id: {book.guestId}," +
+                    $"room Number: {book.roomNumber}," +
+                    $"checkInDate: {book.checkInDate}," +
+                    $"checkOutDate: {book.checkOutDate}," +
+                    $"totalPrice: {book.totalPrice}," +
+                    $"status: {book.status}");
+            }
+        }
     }
 }
