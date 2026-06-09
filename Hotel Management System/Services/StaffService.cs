12d803134c54fc40169bf52245919f846c7e62db
“Anoud <anoudalsaidi1993@gmail.com>
2026-06-09 12:03:16 +0400

    

--- Hotel Management System/Services/StaffService.cs
+++ Hotel Management System/Services/StaffService.cs
@@ -1,5 +1,6 @@
 ﻿using Hotel_Management_System.Models;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,9 +14,46 @@ namespace Hotel_Management_System.Services
         {
             foreach (StaffModel staff in stt)
             {
-                Console.WriteLine($"");
+                Console.WriteLine($"staff Id{staff.staffId}," +
+                    $"full Name:{staff.fullName}," +
+                    $"role:{staff.role}," +
+                    $"is on Duty:{staff.isOnDuty}");
             }
 
         }
+
+        public static string FindStaffById(List<StaffModel> staf, string staffid)
+        {
+            foreach (StaffModel staff in staf)
+            {
+                if (staff.staffId == staffid)
+                {
+                    return staffid;
+                }
+                else
+                {
+                    Console.WriteLine("No matching ");
+                }
+               
+            }
+            return staffid;
+        }
+
+
+        public static void ToggleDutyStatus(StaffModel duty)
+        {
+          
+            if(duty.isOnDuty == true)
+            {
+                duty.isOnDuty = false;
+            }
+            else
+            {
+                duty.isOnDuty = true;
+            }
+           
+        }
+
+
     }
 }
